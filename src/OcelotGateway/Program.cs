02909a30239dc6e9ce2934ad19a9e257baa455d6
Lewis <zlzforever@163.com>
2018-04-02 14:19:15 +0800

    start our api gateway

---
+++ src/OcelotGateway/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OcelotGateway
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args)
+		{
+
+			IWebHostBuilder builder = new WebHostBuilder();
+
+			//注入WebHostBuilder
+			return builder.ConfigureServices(service =>
+				{
+					service.AddSingleton(builder);
+				})
+
+				//加载configuration配置文人年
+				.ConfigureAppConfiguration(conbuilder =>
+				{
+					conbuilder.AddJsonFile("configuration.json");
+				})
+				.UseKestrel()
+				.UseUrls("http://*:5000")
+				.UseStartup<Startup>()
+				.Build();
+		}
+	}
+}
